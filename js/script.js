cd8753d9a9cfc290182e8814cdc0c41e846f5506
Joselin Zarate <zaratejoselin594@gmail.com>
2023-08-13 19:18:02 -0300

    portafolio

--- js/script.js
+++ js/script.js
@@ -1,7 +1,6 @@
 let progress = document.querySelector(".contenedor-progreso")
 
 window.onload = function () {
-  progress.setAttribute("class", "barraProgress")
   setTimeout(() => {
     progress.removeAttribute("class", "barraProgress")
     progress.setAttribute("class", "contenedor-progreso")
